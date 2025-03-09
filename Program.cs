3dd7f6b4c8a8eda488df7dfa8f37e804862cf595
dherbert01 <dwayneh01@icloud.com>
2025-03-09 22:51:41 +0000

    testing something in .net only

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static libs.Class1;
+
+Console.WriteLine("Hello, World!");
+
+libfunc1();
---
+++ libs/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace libs;
+public class Class1
+{
+    public static void libfunc1() {
+        Console.WriteLine("running from libfunc1");
+    }
+}
