fb909034985cc72eb7f749f1c2c7d0d65a9573c2
dherbert01 <dwayneh01@icloud.com>
2025-03-12 19:40:59 +0000

    reverted to 3dd7f6b4c8a8eda488df7dfa8f37e804862cf595

--- Program.cs
+++ Program.cs
@@ -1,33 +1,6 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using web.Data;
+﻿// See https://aka.ms/new-console-template for more information
+using static libs.Class1;
 
-var builder = WebApplication.CreateBuilder(args);
+Console.WriteLine("Hello, World!");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-} 
-
-//testing trigger
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+libfunc1();
