54ecf4a27140e86366dd8d9a712cbaae43aaef68
dherbert01 <dwayneh01@icloud.com>
2025-03-16 12:07:19 +0000

    logging work

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -1,5 +1,7 @@
-namespace openAPIdotnet.Data;
 
+using System.Diagnostics;
+
+namespace openAPIdotnet.Data;
 public class WeatherForecastService
 {
     private static readonly string[] Summaries = new[]
@@ -7,10 +9,27 @@ public class WeatherForecastService
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private readonly ILogger<WeatherForecastService> _logger;
+
+    public WeatherForecastService(ILogger<WeatherForecastService> logger)
+    {
+        _logger = logger;
+    }
+
     public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
     {
+
+        _logger.LogInformation("Weather data fetched visited at {DT}", 
+        DateTime.UtcNow.ToLongTimeString());
+        _logger.LogError("log error at {DT}", 
+        DateTime.UtcNow.ToLongTimeString());
+        _logger.LogCritical("log critical visited at {DT}", 
+        DateTime.UtcNow.ToLongTimeString());
+
         return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
+            
+
             Date = startDate.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
